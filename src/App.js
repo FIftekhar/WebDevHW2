be191a84feabeeccea6b9fb3c8387d916d23245a
fiftekhar <iftekhar.f.19@gmail.com>
2022-11-21 23:33:13 -0500

    making base for app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>The Flags Memory Game</h1>
+			<button>New Game</button>
+		</div>
+	);
 }
 
 export default App;
